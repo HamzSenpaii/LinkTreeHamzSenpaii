9cf8314aff8c271756200c98c49a49638a0d4e7d
HamzSenpaii <ucokya17@gmail.com>
2024-10-18 05:27:26 +0700

    LinkTreeHamzSenpaii

---
+++ script.js
@@ -0,0 +1,157 @@
+actionLink.forEach((action) => {
+  action.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
+
+    /**
+     * Untuk memunculkan toast notification
+     */
+    document.getElementById("toast").innerHTML = `
+        <div class="toast-container">
+            <p>✅ Link <strong> ${action.parentElement.innerText} </strong> berhasil disalin!</p>
+        </div>
+    `;
+
+    /**
+     * Untuk menghilangkan toast notification
+     */
+
+    setTimeout(() => {
+      document
+        .querySelector("#toast .toast-container")
+        .classList.add("toast-gone");
+    }, 300);
+
+    setTimeout(() => {
+      document.querySelector("#toast .toast-container").remove();
+    }, 2000);
+  });
+});
+
+/**
+ * Untuk ganti icon sosmed saat hover
+ */
+
+document.querySelectorAll(".sosmed i").forEach((sosmed) => {
+  sosmed.addEventListener("mouseenter", () => {
+    sosmed.classList.remove("ph");
+    sosmed.classList.add("ph-fill");
+  });
+
+  sosmed.addEventListener("mouseleave", () => {
+    sosmed.classList.remove("ph-fill");
+    sosmed.classList.add("ph");
+  });
+});
+
+
+        particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 80,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200,
+                        "duration": 0.4
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
+        });
